6cfe874427a71c06792a2ff36aabe708a9160c88
habibshrabon <snhabib777@gmail.com>
2022-01-27 23:49:22 +0600

    some added

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://travelersblog:BdsboI7beTfkJTGy@cluster0.e49g7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("travel-blog");
+    const blogCollection = database.collection("blogs");
+
+    //add api
+    app.post("/blogs", async (req, res) => {
+      const product = req.body;
+      const result = await blogCollection.insertOne(product);
+      res.json(result);
+    });
+
+    //get products api
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find({});
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
+  } finally {
+    //  await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send(" server is running");
+});
+
+app.listen(port, () => {
+  console.log("server running at port", port);
+});
